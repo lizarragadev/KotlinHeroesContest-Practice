c884a34c511a7dd211b033fb8a4e9572b38afed5
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-02-23 23:37:34 -0400

    Add problem A solution

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,40 +1,12 @@
 import java.io.PrintWriter
 import java.util.*
 
-@JvmField val INPUT = System.`in`
-@JvmField val OUTPUT = System.out
-
-@JvmField val _reader = INPUT.bufferedReader()
-fun readLine(): String? = _reader.readLine()
-fun readLn() = _reader.readLine()!!
-@JvmField var _tokenizer: StringTokenizer = StringTokenizer("")
-fun read(): String {
-    while (_tokenizer.hasMoreTokens().not()) _tokenizer = StringTokenizer(_reader.readLine() ?: return "", " ")
-    return _tokenizer.nextToken()
-}
-fun readInt() = read().toInt()
-fun readDouble() = read().toDouble()
-fun readLong() = read().toLong()
-fun readStrings(n: Int) = List(n) { read() }
-fun readLines(n: Int) = List(n) { readLn() }
-fun readInts(n: Int) = List(n) { read().toInt() }
-fun readIntArray(n: Int) = IntArray(n) { read().toInt() }
-fun readDoubles(n: Int) = List(n) { read().toDouble() }
-fun readDoubleArray(n: Int) = DoubleArray(n) { read().toDouble() }
-fun readLongs(n: Int) = List(n) { read().toLong() }
-fun readLongArray(n: Int) = LongArray(n) { read().toLong() }
-
-@JvmField val _writer = PrintWriter(OUTPUT, false)
-inline fun output(block: PrintWriter.() -> Unit) { _writer.apply(block).flush() }
-
-/********************** CODE *********************/
-
 fun main(args: Array<String>) {
-    _writer.solve()
+    _writer.problemA()
     _writer.flush()
 }
 
-fun PrintWriter.solve() {
+fun PrintWriter.problemA() {
     val t = readInt()
     if(t in 1..100) {
         for (i in 1..t) {
@@ -69,5 +41,3 @@ fun findPosition(a: MutableList<Int>): Int {
     }
     return position + 1
 }
-
-/********************** CODE *********************/
