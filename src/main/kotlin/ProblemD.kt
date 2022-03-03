969789e6e520a582dfaf8a6313d9bc1cdfea45e9
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-03-02 20:17:34 -0400

    Solved problem D

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -1,7 +1,13 @@
 import java.io.PrintWriter
-import java.util.*
-import java.util.stream.Collectors
 import kotlin.math.pow
+
+val responseAffirmetive = "YES"
+val responseNegative = "NO"
+val a = mutableListOf<Int>()
+val asc = mutableListOf<Int>()
+val desc = mutableListOf<Int>()
+var invalid = false
+var repeated = -1
 
 fun main(args: Array<String>) {
     _writer.problemD()
@@ -11,26 +17,47 @@ fun main(args: Array<String>) {
 fun PrintWriter.problemD() {
     val n = readInt()
     if (n in 1..((2*10.toDouble().pow(5.toDouble())).toInt())) {
-        val a = fillListD(n)
+        fillListD(n)
         if(a.size > 1) {
-            a.sort()
-            val asc = mutableListOf<Int>()
-            val desc = mutableListOf<Int>()
-            for (i in a) {
-                // iterar uno a uno con el siguiente y ver si son iguales
+            a.sortDescending()
+            while(a.isNotEmpty()) {
+                if(a[0] != repeated) {
+                    if(a.size != 1) {
+                        if(a[0] == a[1]) {
+                            repeated = a[0]
+                            asc.add(a[0])
+                            a.removeAt(0)
+                        }
+                    }
+                    desc.add(a[0])
+                    a.removeAt(0)
+                } else {
+                    invalid = true
+                    break
+                }
             }
+            printResponse()
         } else {
-            // imprimir res de 1
+            desc.add(a[0])
+            printResponse()
         }
-
-        //val set = a.stream().distinct().collect(Collectors.toList())
-
-
     }
 }
 
-fun fillListD(n: Int) : MutableList<Int> {
-    val a = mutableListOf<Int>()
+fun printResponse() {
+    if(!invalid) {
+        println(responseAffirmetive)
+        println(asc.size)
+        asc.sort()
+        asc.forEach { print("$it ") }
+        println("\n${desc.size}")
+        //desc.sortDescending()
+        if(desc.isEmpty()) println("") else  desc.forEach { print("$it ") }
+    } else {
+        print(responseNegative)
+    }
+}
+
+fun fillListD(n: Int) {
     for (i in 1..n) a.add(readInt())
-    return a
 }
